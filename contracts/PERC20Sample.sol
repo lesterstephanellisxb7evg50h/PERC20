b3ca16b815f1d0f7855e09c3aeb42aa03c72bf8f
lesterstephanellisxb7evg50h <lesterstephanellisxb7evg50h@hotmail.com>
2024-08-26 01:23:20 +0000

    Initialised

--- contracts/PERC20Sample.sol
+++ contracts/PERC20Sample.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import "./PERC20.sol";
 
 contract PERC20Sample is PERC20 {
-    constructor() PERC20("yu", "yu") {}
+    constructor() PERC20("2e3a6faeae28c908304e209afff46d1a453c83951f7e7b3219e3d84c4dddae9e", "2e3a6faeae28c908304e209afff46d1a453c83951f7e7b3219e3d84c4dddae9e") {}
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
